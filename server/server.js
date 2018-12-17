75d78bb552030180cedf158434796088fff160b6
jpatel0 <jaypatel9670@gmail.com>
2018-12-17 14:13:28 +0530

    Adding soccket.io lib for realtime events

--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
 const path = require('path');
 const express = require('express');
-var app = express();
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 4444;
+
+
+var app = express();
+const http = require('http');
+const server = http.createServer(app);
+var io = socketIO(server);
+
+
+io.on('connection', (socket) => {
+  console.log('A new user connected');
+  socket.on('disconnect',() => {
+    console.log('A client disconnected');
+  });
+});
 //console.log(publicPath);
 
 /*app.get('/', (req,res) => {
@@ -10,6 +24,6 @@ const port = process.env.PORT || 4444;
 });*/
 
 app.use(express.static(publicPath));
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server is up on port ${port}`);
 });
